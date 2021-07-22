3d9acf77976c0b710f4f01cbae6103f27b2d4f31
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-22 12:12:32 +0200

    traduct

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+/**
+ * @Route("/{_locale}")
+ */
 class TodoController extends AbstractController
 {
     /**
